f3780e14bf04f7e2694ed2c259a03e023167849e
bepoisso <poisson.benjamin69@gmail.com>
2025-03-31 14:57:26 +0200

    CLEAN files

--- mandatory/srcs/main/main.c
+++ mandatory/srcs/main/main.c
@@ -9,11 +9,5 @@ int main(int argc, char *argv[])
 		ft_error("invalid argument\n", true);
 	map = get_file(argv[1]);
 	i = 0;
-	while (map[i])
-	{
-		ft_printf("map[%d] = %s", i, map[i]);
-		i++;
-	}
-	
 	return (0);
 }
