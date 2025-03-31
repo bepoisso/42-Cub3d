295b1263e9b1d77332a025abc07160b4ab1d58b2
bepoisso <poisson.benjamin69@gmail.com>
2025-03-31 12:11:36 +0200

    ADD get line

--- mandatory/includes/cub3d.h
+++ mandatory/includes/cub3d.h
@@ -1,1 +1,17 @@
-/* Include */
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <fcntl.h>
+#include <stdbool.h>
+#include "../libft/includes/libft.h"
+
+/* Color */
+# define BLUE "\001\033[0;34m\002"
+# define RED "\001\033[0;31m\002"
+# define GREEN "\001\033[0;32m\002"
+# define YELLOW "\001\033[0;33m\002"
+# define CYAN "\001\033[0;36m\002"
+# define VIOLET "\001\033[0;35m\002"
+# define RESET "\001\033[0m\002"
+
+#endif
