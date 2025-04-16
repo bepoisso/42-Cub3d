66fb206820ce876313347cc9042737be88d1c294
Arnaud Fontan <afontan@c2r7p7.42perpignan.fr>
2025-04-16 12:15:56 +0200

    FIX floor cieling

--- mandatory/srcs/tmp_arnaud_texture.c
+++ mandatory/srcs/tmp_arnaud_texture.c
@@ -7,21 +7,6 @@ static float	distance(float x, float y)
 	return (sqrt(x * x + y * y));
 }
 
-static bool is_no_or_so(float ray_x, float ray_y, t_mlx *mlx)
-{
-	int map_x;
-	int map_y;
-	float	x_tile;
-	float	y_tile;
-
-	map_x = (int)(ray_x / TEXTURE);
-	map_y = (int)(ray_y / TEXTURE);
-	x_tile = ray_x - (map_x * TEXTURE);
-	y_tile = ray_y - (map_y * TEXTURE);
-	if (x_tile < 0.0001 || x_tile > TEXTURE - 0.0001)
-		return (false);
-	return (true);
-}
 
 static float	fixed_dist(float x1, float y1, float x2, float y2, t_mlx *mlx)
 {
@@ -43,7 +28,6 @@ void	draw_textured_wall(int ray_x, int ray_y, t_mlx *mlx, t_player *player, int 
 	float	height;
 	int		start_y;
 	int		end;
-	bool	is_ns;
 	t_img	*texture;
 	float	wall_x;
 	int		y;
@@ -55,7 +39,6 @@ void	draw_textured_wall(int ray_x, int ray_y, t_mlx *mlx, t_player *player, int 
 	height = (TEXTURE / dist) * (WIDTH / 2);
 	start_y = (HEIGHT - height) / 2;
 	end = start_y + height;
-	is_ns = is_no_or_so(ray_x, ray_y, mlx);
 	if (side == 1)
 	{
 		if (ray_y < player->y)
@@ -70,7 +53,7 @@ void	draw_textured_wall(int ray_x, int ray_y, t_mlx *mlx, t_player *player, int 
 			texture = mlx->element->we_img;
 		else
 			texture = mlx->element->ea_img;
-			wall_x = ray_y;
+		wall_x = ray_y;
 	}
 	wall_x = fmod(wall_x, TEXTURE);
 	tex_x = (int)(wall_x / TEXTURE * texture->width);
